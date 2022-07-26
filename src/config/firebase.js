e3c382fbea28f8fbf5fbcf3fe08f535e3d4bd640
Zulfahmi321 <zulfahmi1729@gmail.com>
2022-07-27 02:10:08 +0700

    update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,10 @@
 const admin = require("firebase-admin");
-admin.initializeApp();
-
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 module.exports = admin;
+
+// const admin = require("firebase-admin");
+// admin.initializeApp();
+// module.exports = admin;
