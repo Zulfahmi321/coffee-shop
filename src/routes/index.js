8cbde418b79a707d37026b26f415ee64bae0a725
Zulfahmi321 <zulfahmi1729@gmail.com>
2022-07-26 23:41:44 +0700

    add firebase to notif

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,26 @@ const express = require("express");
 
 const Router = express.Router();
 
+
 //import kedalam main router
 const userRouter = require("./user");
 const productRouter = require("./product");
 const transactionRouter = require("./transaction");
 const promosRoutes = require("./promos");
 const authRouter = require("./auth");
+const notifRouter = require("./notification")
 
+Router.get("/", (_, res) => {
+    res.json({
+        message: "Welcome to Project Coffee Shop",
+    });
+});
 // menjalankan Router berdasarkan router yang di import
 Router.use("/user", userRouter);
 Router.use("/product", productRouter);
 Router.use("/transaction", transactionRouter);
 Router.use("/promos", promosRoutes);
 Router.use("/auth", authRouter);
+Router.use("/notif", notifRouter);
 
 module.exports = Router;
