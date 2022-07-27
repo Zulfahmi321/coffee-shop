14d8b4d5bb0889cc09b3b7356b57155cb1fcb42c
Zulfahmi321 <zulfahmi1729@gmail.com>
2022-07-27 07:11:00 +0700

    fix notif

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
